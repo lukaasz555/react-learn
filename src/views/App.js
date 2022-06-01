f5d2fcb77e68314183b6aa96c4ead2223000fc5d
lukaasz555 <97296160+lukaasz555@users.noreply.github.com>
2022-06-01 22:24:53 +0200

    basic setup

--- src/views/App.js
+++ src/views/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
+import logo from 'assets/logo.svg';
 import './App.css';
+
+console.log("asdf");
+
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
