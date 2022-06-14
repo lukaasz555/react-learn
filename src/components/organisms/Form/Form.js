51587ee1a503e52085e3bc173f19184ec6500280
lukaasz555 <97296160+lukaasz555@users.noreply.github.com>
2022-06-14 11:21:08 +0200

    edit handleAddUser at Rootjs

--- src/components/organisms/Form/Form.js
+++ src/components/organisms/Form/Form.js
@@ -9,9 +9,10 @@ const Form = ({ handleAddUser, formValues, handleInputChange }) => {
         <Wrapper as="form" onSubmit={handleAddUser}>
             <StyledTitle>Add new student</StyledTitle>
             <FormField label="Name" id="name" name="name" value={formValues.name} onChange={handleInputChange} />
-            <FormField label="Result" id="result" name="result" value={formValues.result} onChange={handleInputChange} />
+            <FormField label="Result" id="result" name="result" value={formValues.result} onChange={handleInputChange} type="number" />
             <FormField label="Average" id="average" name="average" value={formValues.average} onChange={handleInputChange} />
             <Button type="submit" >Add</Button><br></br>
+            <p></p>
             <Link to='/'>
                 <Button>back</Button>
             </Link>
