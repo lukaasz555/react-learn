13056d548f755b602d9d638b4b7bc65330a222a1
lukaasz555 <97296160+lukaasz555@users.noreply.github.com>
2022-06-20 10:18:57 +0200

    fixed bugs - userprovider

--- src/components/molecules/UsersListItem/UsersListItem.js
+++ src/components/molecules/UsersListItem/UsersListItem.js
@@ -1,30 +1,31 @@
-import React from "react";
+import React, { useContext } from "react";
 import PropTypes from "prop-types";
 import { ReactComponent as DeleteIcon } from "assets/icons/delete-icon.svg";
 import RemoveButton from "components/atoms/RemoveButton/RemoveButton";
 import { Average } from "components/atoms/Average/Average";
 import { Wrapper, ParagraphsWrapper } from "./UsersListItem.styles";
 import { UserShape } from "types";
+import { UsersContext } from "providers/UsersProvider";
 
-const UsersListItem = ({
-  handleDelete,
-  userData: { average = "TBA", name, result },
-}) => (
-  <Wrapper>
-    <Average average={average} />
-    <ParagraphsWrapper>
-      <p>{name}</p>
-      <p>{result}%</p>
-    </ParagraphsWrapper>
-    <RemoveButton onClick={() => handleDelete(name)}>
-      <DeleteIcon />
-    </RemoveButton>
-  </Wrapper>
-);
+const UsersListItem = ({ userData: { average = "TBA", name, result } }) => {
+  const { handleDelete } = useContext(UsersContext);
+
+  return (
+    <Wrapper>
+      <Average average={average} />
+      <ParagraphsWrapper>
+        <p>{name}</p>
+        <p>{result}%</p>
+      </ParagraphsWrapper>
+      <RemoveButton onClick={() => handleDelete(name)}>
+        <DeleteIcon />
+      </RemoveButton>
+    </Wrapper>
+  );
+};
 
 UsersListItem.propTypes = {
   userData: PropTypes.arrayOf(PropTypes.shape(UserShape)),
-  handleDelete: PropTypes.func,
 };
 
 export default UsersListItem;
--- src/views/Root.js
+++ src/views/Root.js
@@ -7,36 +7,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainTemplate from "components/templates/MainTemplate/MainTemplate";
 import AddUser from "./AddUser";
 import Dashboard from "./Dashboard";
-import UsersProvider from "providers/UsersProviders";
-
-/* export const UsersContext = React.createContext({
-  users: [],
-  handleDelete: () => {},
-  handleAddUser: () => {},
-}); */
+import UsersProvider from "providers/UsersProvider";
 
 const Root = () => {
-  /*   const [users, setUsers] = useState(usersData);
-
-  const handleAddUser = (e) => {
-    e.preventDefault();
-    const newUser = {
-      name: formValues.name,
-      result: formValues.result,
-      average: formValues.average,
-    };
- */
-  /*     const infoMsg = document.querySelector("p");
-    if (newUser.name.length >= 3) {
-      setUsers([...users, newUser]);
-      setFormValue(initialFormState);
-      infoMsg.style.color = "green";
-      infoMsg.textContent = "success";
-    } else {
-      infoMsg.style.color = "red";
-      infoMsg.textContent = "name should contain at least 3 chars";
-    } */
-
   return (
     <Router>
       <ThemeProvider theme={theme}>
