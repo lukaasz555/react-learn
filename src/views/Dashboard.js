c9377892b856e201c4f69c504153464e497e9979
lukaasz555 <97296160+lukaasz555@users.noreply.github.com>
2022-10-11 16:34:58 +0200

    Update Dashboard.js

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,46 +1,48 @@
-import React, { useEffect, useState } from "react";
-import { ViewWrapper } from "components/molecules/ViewWrapper/ViewWrapper";
-import UsersList from "components/organisms/UsersList/UsersList";
+import React, { useEffect, useState } from 'react';
+import { ViewWrapper } from 'components/molecules/ViewWrapper/ViewWrapper';
+import UsersList from 'components/organisms/UsersList/UsersList';
 //import { UsersContext } from "providers/UsersProvider";
-import { useParams, NavLink } from "react-router-dom";
-import axios from "axios";
+import { useParams, NavLink } from 'react-router-dom';
+import axios from 'axios';
 
 const Dashboard = () => {
-  //const { users } = useContext(UsersContext);
-  const { id } = useParams();
-  const [students, setStudents] = useState([]);
-  const [groups, setGroups] = useState([]);
+	//const { users } = useContext(UsersContext);
+	const { id } = useParams();
+	const [students, setStudents] = useState([]);
+	const [groups, setGroups] = useState([]);
 
-  useEffect(() => {
-    axios
-      .get("/groups")
-      .then(({ data }) => setGroups(data.groups))
-      .catch((err) => console.log(err));
-  }, []);
+	useEffect(() => {
+		axios
+			.get('/groups')
+			.then(({ data }) => setGroups(data.groups))
+			.catch((err) => console.log(err));
+	}, []);
 
-  useEffect(() => {
-    axios
-      .get(`/students/${id || groups[0]}`)
-      .then(({ data }) => setStudents(data.students))
-      .catch((err) => console.log(err));
-  }, [id, groups]);
+	useEffect(() => {
+		axios
+			.get(`/students/${id || groups[0]}`)
+			.then(({ data }) => setStudents(data.students))
+			.catch((err) => console.log(err));
+	}, [id, groups]);
 
-  return (
-    <ViewWrapper>
-      <nav>
-        {groups.length > 0 ? (
-          groups.map((group) => (
-            <NavLink key={group} to={`/group/${group}`}>
-              {group}
-            </NavLink>
-          ))
-        ) : (
-          <p>Brak dostępnych grup</p>
-        )}
-      </nav>
-      <UsersList /* users={users} */ users={students} />
-    </ViewWrapper>
-  );
+	return (
+		<>
+			<nav>
+				{groups.length > 0 ? (
+					groups.map((group) => (
+						<NavLink key={group} to={`/group/${group}`}>
+							{group}
+						</NavLink>
+					))
+				) : (
+					<p>Brak dostępnych grup</p>
+				)}
+			</nav>
+			<ViewWrapper>
+				<UsersList /* users={users} */ users={students} />
+			</ViewWrapper>
+		</>
+	);
 };
 
 export default Dashboard;
